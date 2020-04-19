0b94c137dffcbfce0ab237b8bc9e09e2dcb5dfaa
mgsong <mgsong@jjsoft.kr>
2020-04-19 17:34:05 +0900

    setting of mobx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import CounterStore from './stores/counter'
+import { Provider } from 'mobx-react' // MobX 에서 사용하는 Provider
+
+const counter = new CounterStore() // 스토어 인스턴스를 만들고
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider counter={counter}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
